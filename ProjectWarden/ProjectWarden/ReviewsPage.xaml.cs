45a0443fa93dd872f2f27302d20208cd25f598ce
Holgenberg <alexeiholgate1@gmail.com>
2019-08-04 15:27:59 +0100

    Created Reviews Page

---
+++ ProjectWarden/ProjectWarden/ReviewsPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using ProjectWarden.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjectWarden
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ReviewsPage : ContentPage
+	{
+        public AddressListing ReviewsAddressListing { get; set; }
+
+        public ReviewsPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
